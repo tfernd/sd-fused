fix time-embedding bug and half-precision (dtype)

sd/app/sd.py
@@ -22,6 +22,9 @@ MAGIC = 0.18215
 class StableDiffusion:
     low_ram: bool = False
 
+    device: torch.device = torch.device("cpu")
+    dtype: torch.dtype = torch.float32
+
     def __init__(self, path: str | Path) -> None:
         path = Path(path)
 
@@ -40,6 +43,8 @@ class StableDiffusion:
     def cuda(self) -> Self:
         clear_cuda()
 
+        self.device = torch.device("cuda")
+
         self.unet.cuda()
         self.vae.cuda()
 
@@ -54,6 +59,8 @@ class StableDiffusion:
     def half(self) -> Self:
         self.unet.half()
         self.vae.half()
+
+        self.dtype = torch.float16
 
         return self
 
@@ -75,19 +82,18 @@ class StableDiffusion:
         timesteps: list[int] = scheduler.timesteps.tolist()
 
         # prompt
-        text_emb = self.clip(prompt) if prompt is not None else None
-        neg_emb = self.clip(negative_prompt)
-        if text_emb is not None:
-            assert text_emb.shape == neg_emb.shape
-
+        neg_emb = self.clip(negative_prompt).to(self.device).to(self.dtype)
+        if prompt is not None:
+            text_emb = self.clip(prompt).to(self.device).to(self.dtype)
             context = (text_emb, neg_emb)
         else:
             context = neg_emb
 
-        # seed
+        # seed and noise
         seed = seed or random.randint(0, 2 ** 16)
         torch.manual_seed(seed)
         latents = torch.randn(1, 4, height // 8, width // 8)
+        latents = latents.to(self.device).to(self.dtype)
 
         # generation
         clear_cuda()

sd/layers/attention/cross_attention.py
@@ -75,7 +75,8 @@ class CrossAttention(nn.Module):
             return self.to_out(x)
 
         # split-attention score
-        x = torch.empty(*q.shape[0:2], v.shape[2], device=q.device, dtype=q.dtype)
+        shape = (*q.shape[0:2], v.shape[2])
+        x = torch.zeros(shape, device=q.device, dtype=q.dtype)
         for i in range(0, k.shape[0], self.split_attention_chunks):
             idx = slice(i, i + self.split_attention_chunks)
 

sd/layers/base/group_norm.py
@@ -2,10 +2,8 @@ from __future__ import annotations
 
 import torch.nn as nn
 
-from .ops import Ops
 
-
-class GroupNorm(Ops, nn.GroupNorm):
+class GroupNorm(nn.GroupNorm):
     def __init__(
         self,
         num_groups: int,

sd/layers/base/layer_norm.py
@@ -3,10 +3,7 @@ from __future__ import annotations
 import torch.nn as nn
 
 
-
-from .ops import Ops
-
-class LayerNorm(Ops, nn.LayerNorm):
+class LayerNorm(nn.LayerNorm):
     def __init__(
         self,
         normalized_shape: int,

sd/layers/embedding/time_steps.py
@@ -62,5 +62,5 @@ class Timesteps(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         x = x[..., None]
 
-        return x.mul(self.freq).add_(self.phase).mul_(self.amplitude)
+        return x.mul(self.freq).add_(self.phase).sin_().mul_(self.amplitude)
         # return self.amplitude * torch.sin(x * self.freq + self.phase)

sd/models/ae_kl.py
@@ -9,12 +9,13 @@ import torch.nn as nn
 from torch import Tensor
 
 from ..utils import normalize, denormalize
-from ..layers.base import Conv2d, HalfWeightsModel, Ops
+from ..layers.base import Conv2d, HalfWeightsModel
 from ..layers.distribution import DiagonalGaussianDistribution
 from ..layers.auto_encoder.encoder import Encoder
 from ..layers.auto_encoder.decoder import Decoder
 
-class AutoencoderKL(Ops,HalfWeightsModel, nn.Module):
+
+class AutoencoderKL(HalfWeightsModel, nn.Module):
     def __init__(
         self,
         *,
@@ -56,8 +57,6 @@ class AutoencoderKL(Ops,HalfWeightsModel, nn.Module):
 
     @torch.no_grad()
     def encode(self, x: Tensor) -> DiagonalGaussianDistribution:
-        x = x.to(self.dtype).to(self.device)
-
         x = normalize(x)
         x = self.encoder(x)
 
@@ -68,8 +67,6 @@ class AutoencoderKL(Ops,HalfWeightsModel, nn.Module):
 
     @torch.no_grad()
     def decode(self, z: Tensor) -> Tensor:
-        z = z.to(self.dtype).to(self.device)
-
         z = self.post_quant_conv(z)
         out = self.decoder(z)
 

sd/models/unet_conditional.py
@@ -10,7 +10,7 @@ from torch import Tensor
 
 from ..layers.embedding import Timesteps, TimestepEmbedding
 from ..layers.activation import SiLU
-from ..layers.base import Conv2d, GroupNorm, HalfWeightsModel, Ops
+from ..layers.base import Conv2d, GroupNorm, HalfWeightsModel
 from ..layers.blocks import (
     UNetMidBlock2DCrossAttn,
     DownBlock2D,
@@ -21,7 +21,7 @@ from ..layers.blocks import (
 from ..layers.attention import CrossAttention
 
 
-class UNet2DConditional(Ops,HalfWeightsModel, nn.Module):
+class UNet2DConditional(HalfWeightsModel, nn.Module):
     def __init__(
         self,
         *,
@@ -168,11 +168,8 @@ class UNet2DConditional(Ops,HalfWeightsModel, nn.Module):
     def forward(self, x: Tensor, timestep: int, *, context: Tensor) -> Tensor:
         B, C, H, W = x.shape
 
-        x = x.to(self.dtype).to(self.device)
-        context = context.to(self.dtype).to(self.device)
-
         # 1. time embedding
-        timesteps = torch.tensor([timestep], device=self.device)
+        timesteps = torch.tensor([timestep], device=x.device)
         timesteps = timesteps.expand(B)
 
         temb = self.time_proj(timesteps)