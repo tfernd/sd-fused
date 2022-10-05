b5a19b4189748c617b832844a232382b910fef59
Thales Fernandes <ThalesFDFernandes@gmai.com>
2022-10-05 16:08:19 +0200

    image2tensor

--- sd/utils/image2tensor.py
+++ sd/utils/image2tensor.py
@@ -15,7 +15,7 @@ from einops import rearrange
 def image2tensor(
     path: str | Path, *, resize: Optional[int | tuple[int, int]] = None,
 ) -> Tensor:
-    """Open's an image as pytorch batched-Tensor (B C H W)."""
+    """Open an image as pytorch batched-Tensor (B C H W)."""
 
     img = Image.open(path).convert("RGB")
     if resize is not None:
