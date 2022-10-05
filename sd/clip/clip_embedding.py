e366533cd5256588f9140afc9184339e90ebdb67
Thales Fernandes <ThalesFDFernandes@gmai.com>
2022-10-05 16:10:34 +0200

    ClipEmbedding

--- sd/clip/clip_embedding.py
+++ sd/clip/clip_embedding.py
@@ -13,6 +13,8 @@ from transformers.models.clip.tokenization_clip import CLIPTokenizer
 
 
 class ClipEmbedding:
+    """Convert a text to embeddings using a CLIP model."""
+
     def __init__(
         self, tokenizer_path: str | Path, text_encoder_path: str | Path
     ) -> None:
@@ -21,6 +23,8 @@ class ClipEmbedding:
 
     @staticmethod
     def parse_text(text: str) -> str:
+        """Clean-up spaces/return characters."""
+
         text = text.replace("\n", " ")
         text = re.sub(r"[ ]+", r" ", text)
         text = text.strip()
@@ -30,6 +34,8 @@ class ClipEmbedding:
     @lru_cache(maxsize=None)
     @torch.no_grad()
     def create_embedding(self, text: str) -> Tensor:
+        """Creates an embedding for a text and cache it."""
+
         kwargs = dict(
             padding="max_length", truncation=True, return_tensors="pt"
         )
@@ -45,6 +51,8 @@ class ClipEmbedding:
         device: Optional[torch.device] = None,
         dtype: Optional[torch.dtype] = None,
     ) -> Tensor:
+        """Creates embeddings for a text and send to the correct device/dtype."""
+
         if isinstance(text, str):
             out = self.create_embedding(text)
         else:
