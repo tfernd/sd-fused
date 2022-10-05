5d6f40e227b2356b41588b7405663a09ba19a377
Thales Fernandes <ThalesFDFernandes@gmai.com>
2022-10-05 16:20:26 +0200

    InPlace

--- sd/layers/base/in_place.py
+++ sd/layers/base/in_place.py
@@ -5,10 +5,15 @@ import torch.nn as nn
 
 
 class InPlace(nn.Module):
+    """Use in-place operations to save memory."""
+
     inplace: bool = True
 
 
 class InPlaceModel(nn.Module):
+    # TODO docstring duplicates
+    """Use in-place operations to save memory."""
+    
     def set_inplace(self, inplace: bool = False) -> Self:
         for name, module in self.named_modules():
             if isinstance(module, InPlace):
