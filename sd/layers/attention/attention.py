54d21a1a5958d3d5c96c7075a15e5151ffef3dce
Thales Fernandes <ThalesFDFernandes@gmai.com>
2022-10-13 13:49:59 +0200

    attention assert

--- sd/layers/attention/attention.py
+++ sd/layers/attention/attention.py
@@ -9,10 +9,14 @@ from ...utils import softmax
 
 def attention(
     q: Tensor,  # (B, T, C)
-    k: Tensor,  # (B, T, C')
+    k: Tensor,  # (B, T', C)
     v: Tensor,  # (B, T', C)
     chunks: Optional[int] = None,
 ) -> Tensor:
+    assert q.shape[0] == k.shape[0] == v.shape[0]
+    assert q.shape[2] == k.shape[2] == v.shape[2]
+    assert k.shape[1] == v.shape[1]
+
     k = k.transpose(1, 2)
 
     if chunks is None:
@@ -32,3 +36,10 @@ def attention(
         del attn
 
     return out
+
+
+q = torch.randn(8, 1024, 80)
+k = torch.randn(8, 1024, 80)
+v = torch.randn(8, 1024, 80)
+out = attention(q, k, v)
+out.shape
