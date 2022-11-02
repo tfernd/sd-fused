cc69ed31e474f68fb41084d8d79486bfcd196d3e
Thales Fernandes <ThalesFDFernandes@gmai.com>
2022-11-02 19:00:08 +0100

    jiggle test

---
+++ sd_fused/utils/jiggle_prompt.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+import re
+import random
+
+
+def jiggle_prompt(prompt: str, vmin: float = 0, vmax: float = 1) -> str:
+    assert ":" not in prompt
+
+    words: list[str] = []
+    for word in re.split(r"(\W)", prompt):
+        if re.match(r"\w", word):
+            v = random.uniform(vmin, vmax)
+            word = f"{word}:{v:.4}"
+        words.append(word)
+
+    return "".join(words)
