fix wrong order

sd_fused/app/setup.py
@@ -20,7 +20,12 @@ class Setup(Base, Properties):
 
         return self
 
-    def to(self, *, device: Optional[str | torch.device] = None, dtype: Optional[torch.dtype] = None,) -> Self:
+    def to(
+        self,
+        *,
+        device: Optional[str | torch.device] = None,
+        dtype: Optional[torch.dtype] = None,
+    ) -> Self:
         if device is not None:
             self.device = torch.device(device)
         if dtype is not None:

sd_fused/functional/normalize.py
@@ -5,7 +5,10 @@ import torch
 from torch import Tensor
 
 
-def normalize(data: Tensor, dtype: Optional[torch.dtype] = None,) -> Tensor:
+def normalize(
+    data: Tensor,
+    dtype: Optional[torch.dtype] = None,
+) -> Tensor:
     """Normalize a byte-Tensor to the [-1, 1] range."""
 
     assert data.dtype == torch.uint8

sd_fused/layers/base/module.py
@@ -75,10 +75,10 @@ class Module(Base, ABC):
 
         if isinstance(state, (str, Path)):
             state = Path(state)
-            assert state.suffix == '.pt'
+            assert state.suffix == ".pt"
 
-            w: dict[str, Tensor] = torch.load(state, map_location='cpu')
-            state = w # ! to make linter happy...
+            w: dict[str, Tensor] = torch.load(state, map_location="cpu")
+            state = w  # ! to make linter happy...
 
         current_state = self.state_dict()
         assert len(current_state) == len(state)
@@ -101,11 +101,11 @@ class Module(Base, ABC):
     @classmethod
     def from_config(cls, path: str | Path) -> Self:
         path = Path(path)
-        assert path.suffix == '.json'
+        assert path.suffix == ".json"
 
-        with open(path, 'r', encoding='UTF-8') as handle:
+        with open(path, "r", encoding="UTF-8") as handle:
             kwargs = json.load(handle)
-        
+
         return cls(**kwargs)
 
     def to(